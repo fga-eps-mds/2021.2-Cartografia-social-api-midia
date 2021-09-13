b3c2641bfbfb64d1308d0ab16f14be6a65da2124
Leonardo Barreiros <leossb36@gmail.com>
2021-09-13 20:59:33 -0300

    [#6] refactor - change config module import
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MidiaModule } from './midia/midia.module';
+import { Cloudinary } from './cloudinary/cloudinary';
+import { ConfigService } from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MidiaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MidiaModule],
+  providers: [ConfigService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/midia/midia.module.ts
+++ src/midia/midia.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MidiaService } from './midia.service';
 import { MidiaController } from './midia.controller';
+import { ConfigService } from 'src/config/configuration';
+import { Cloudinary } from 'src/cloudinary/cloudinary';
 
 @Module({
   controllers: [MidiaController],
-  providers: [MidiaService],
+  providers: [MidiaService, Cloudinary, {
+    provide: 'CONFIG',
+    useClass: ConfigService
+  },
+  ],
 })
-export class MidiaModule {}
+export class MidiaModule { }
