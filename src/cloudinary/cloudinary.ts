0a76d5b2d90b65411468c1a54a3da7fb55ceac30
Guilherme Guy <guilhermeguy349@gmail.com>
2021-09-22 18:11:33 -0300

    [#10] feat - Set auto type detection on cloudinart
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/cloudinary/cloudinary.ts
+++ src/cloudinary/cloudinary.ts
@@ -13,10 +13,13 @@ export class Cloudinary {
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream((error, result) => {
-        if (error) return reject(error);
-        resolve(result);
-      });
+      const upload = v2.uploader.upload_stream(
+        { resource_type: 'auto' },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
 
       toStream(file.buffer).pipe(upload);
     });
